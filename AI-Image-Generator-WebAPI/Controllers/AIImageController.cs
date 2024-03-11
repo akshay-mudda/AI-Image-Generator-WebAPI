5e268291a1d8353118ea6beeffdf6e29f94a140c
akshaymu <akshaymu@ICS-LT-66Q1ZD3>
2024-03-11 10:08:45 +0530

    Add project files.

---
+++ AI-Image-Generator-WebAPI/Controllers/AIImageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI.Images;
+using OpenAI;
+
+namespace AI_Image_Generator_WebAPI.Controllers
+{
+
+    [ApiController]
+    public class AIImageController : ControllerBase
+    {
+        [Route("GenerateAIImage")]
+        [HttpGet]
+
+        public async Task<IActionResult> GenerateAIImage(string prompt , int count)
+        {
+            List<string> lstImg = new List<string>();
+
+            string ApiKey = "YOUR_API_KEY_HERE";
+
+            var _openAIClient = new OpenAIClient(new OpenAIAuthentication(ApiKey));
+            var imageResult = await _openAIClient.ImagesEndPoint.GenerateImageAsync(prompt, count, ImageSize.Small);
+            foreach (var image in imageResult)
+            {
+                lstImg.Add(image);
+            }
+            return Ok(lstImg);
+        }
+    }
+}
